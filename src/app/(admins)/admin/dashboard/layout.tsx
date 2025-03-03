b1ba5d5aac8e097cd6ad35708c3bd49b0a5aa9db
mdnuruzzamanemon <mdnuruzzamanemon@gmail.com>
2025-03-03 16:17:30 +0600

    darks mood enabled

--- src/app/(admins)/admin/dashboard/layout.tsx
+++ src/app/(admins)/admin/dashboard/layout.tsx
@@ -9,6 +9,8 @@ import { TrendingCoursesProvider } from "@/app/(admins)/admin/context/TrendingCo
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
 
+import {ModeToggle} from "@/app/(admins)/admin/components/ThemeToggleBtn";
+
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
   
   const cokieStore = await cookies();
@@ -25,10 +27,15 @@ export default async function DashboardLayout({ children }: { children: React.Re
             <AppSidebar />
             <SidebarInset>
               <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+                <div className="flex justify-between w-full">
                 <div className="flex items-center gap-2 px-4">
                   <SidebarTrigger />
                   <Separator orientation="vertical" className="mr-2 h-4" />
                   <MyBreadcrumb />
+                </div>
+                <div className="me-4">
+                <ModeToggle />
+                </div>
                 </div>
               </header>
               <TrendingCoursesProvider>
